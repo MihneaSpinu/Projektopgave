cbaa73db84e1d7221295d13a844308f1935507af
Xip <casp578b@mail.hansenberg.dk>
2024-01-15 13:14:53 +0100

    Added Enemy spawner
    Enemy spawner feature added, implementation is WIP

---
+++ Projektopgave Programering/Assets/Script/EnemySpawner.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    
+    public GameObject enemyPrefab;
+    public bool isSpawning = true;
+    
+    public float enemyInterval = 1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(spawnEnemy(enemyInterval, enemyPrefab));
+    }
+
+    private IEnumerator spawnEnemy(float interval, GameObject enemy)
+    {
+        if (!isSpawning) yield break;
+        yield return new WaitForSeconds(interval);
+        GameObject newEnemy = Instantiate(enemy, new Vector3(Random.Range(-0.1f, 1), 1f,Random.Range(-0.1f, 1)), Quaternion.identity);
+        StartCoroutine(spawnEnemy(interval, enemy));
+    }
+}
