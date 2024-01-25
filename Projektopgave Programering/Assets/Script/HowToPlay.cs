d0b3255cb9640734f532d5f8de6b16614572cf8f
Samuel <samuel@families.dk>
2024-01-25 13:38:34 +0100

    how to play and menus

---
+++ Projektopgave Programering/Assets/Script/HowToPlay.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class HowToPlay : MonoBehaviour
+{
+    public void tutorial()
+    {
+        SceneManager.LoadScene("TutorialScreen");
+    }
+}
