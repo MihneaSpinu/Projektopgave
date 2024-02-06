086551f2b5469818199a18db967d2ea50f80860d
Mihnea <mihn0006@mail.hansenberg.dk>
2024-02-06 13:12:45 +0100

    UI health bar

--- Projektopgave Programering/Assets/Script/Health.cs
+++ Projektopgave Programering/Assets/Script/Health.cs
@@ -2,14 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UIElements;
 
 public class Health : MonoBehaviour
 {
-    [SerializeField] private int health = 100;
+    [SerializeField] public static float health = 100;
     [SerializeField] private float damageDelay = 1f; // delay in seconds
     [SerializeField] private int damage = 10;
     private bool canTakeDamage = true;
-    
+
     // Start is called before the first frame update
     void Start()
     {
@@ -19,10 +20,14 @@ public class Health : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
+        
+        if (Input.GetKeyDown(KeyCode.G))
+        {
+            TakeDamage(10);
+        }
     }
 
-private void OnTriggerEnter(Collider other) 
+    private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("enemy") && canTakeDamage)
         {
@@ -53,7 +58,7 @@ private void OnTriggerEnter(Collider other)
         SceneManager.LoadScene("GameOver");
     }
 
-    public int GetHealth()
+    public float GetHealth()
     {
         return health;
     }
